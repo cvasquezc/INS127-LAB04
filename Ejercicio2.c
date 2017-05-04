fb9c08119cc102068d1350c0ae83c15647cdae9f
Claudio Vásquez <c.vasquezcampos@uandresbello.edu>
2017-05-04 20:59:39 -0300

    Add files via upload

---
+++ Ejercicio2.c
@@ -0,0 +1,26 @@
+// Lab 2 - Ejercicio 2 - Programa para multiplicar dos numeros ocupando sumas
+
+#include <stdio.h>
+
+int multiplicarConSuma(int a, int b){
+		int product=0;
+		if (a>0){
+			product=multiplicarConSuma(a-1,b);
+			product=product+b;
+		return product;
+	}else
+	{
+		return product;
+	}
+}
+int main(int argc, char const *argv[])
+{
+	int numA,numB,producto;
+	printf("Ingrese un numero: \n");
+	scanf("%d",&numA);
+	printf("Ingrese un segundo numero: \n");
+	scanf("%d",&numB);
+	producto = multiplicarConSuma(numA,numB);
+	printf("El producto de los numeros %d y %d es = %d \n",numA,numB,producto);
+	return 0;
+}
